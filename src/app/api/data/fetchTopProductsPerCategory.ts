2afb756dd8c7f8fa48309ecf3f8762472dc17564
Vitor Cardoso <vitor.mauricioc@outlook.com>
2025-05-12 15:45:15 -0300

    test: log resposta api

--- src/app/api/data/fetchTopProductsPerCategory.ts
+++ src/app/api/data/fetchTopProductsPerCategory.ts
@@ -3,7 +3,7 @@ export async function fetchTopProductsPerCategory() {
     const res = await fetch('https://market-pulse-one.vercel.app/api/top-products', {
       next: { revalidate: 600 },
     });
-
+    console.log("resssssssss:", res)
     if (!res.ok) throw new Error('Erro ao buscar top produtos por categoria');
 
     return res.json();
