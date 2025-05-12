c51c47f1684102b2aad667777427f60fefbfb100
Vitor Cardoso <vitor.mauricioc@outlook.com>
2025-05-12 17:47:26 -0300

    ajuste de produtos em trend

--- src/app/dashboard/product-trends/ProductTrendsClient.tsx
+++ src/app/dashboard/product-trends/ProductTrendsClient.tsx
@@ -26,26 +26,26 @@ export default function ProductTrendsClient({ topProducts }: Props) {
         Tendências
       </Typography>
       <Typography mb={3}>
-        Buscas que mais cresceram:
+        <strong>Buscas que mais cresceram:</strong>
       </Typography>
       <Grid container spacing={3} mb={4}>
-        {chartBox('1º MAIOR CRESCIMENTO', 'Teclado', 'sells-icon')}
-        {chartBox('2º MAIOR CRESCIMENTO', 'Cooler', 'sells-icon')}
-        {chartBox('3º MAIOR CRESCIMENTO', 'Moto elétrica', 'sells-icon')}
+        {chartBox('1º MAIOR CRESCIMENTO', 'Mini Compressor Digital', 'sells-icon')}
+        {chartBox('2º MAIOR CRESCIMENTO', 'Creme Multirreparador', 'sells-icon')}
+        {chartBox('3º MAIOR CRESCIMENTO', 'Moletom Liso Algodão', 'sells-icon')}
       </Grid>
 
       <Typography mb={3}>
-        As buscas mais desejadas:
+      <strong>As buscas mais desejadas:</strong>
       </Typography>
       <Grid container spacing={3} mb={4}>
-        {chartBox('1º MAIS DESEJADA', 'Internet Starlink', 'stock-icon')}
-        {chartBox('2º MAIS DESEJADA', 'Creatina Growth', 'stock-icon')}
-        {chartBox('3º MAIS DESEJADA', 'Playstation 5', 'stock-icon')}
+        {chartBox('1º MAIS DESEJADA', 'Fritadeira sem óleo', 'stock-icon')}
+        {chartBox('2º MAIS DESEJADA', 'Creatina Monohidratada', 'stock-icon')}
+        {chartBox('3º MAIS DESEJADA', 'Cadeira de Escritório', 'stock-icon')}
       </Grid>
 
       <Card sx={{ p: 2, borderRadius: 3 }}>
         <CardContent>
-          <Typography variant="h6" mb={2}><strong>Top 5 produtos por categoria</strong></Typography>
+          <Typography variant="h6" mb={2}><strong>Top 5 Produtos por Categoria</strong></Typography>
             <List component="ol" sx={{ listStyleType: 'decimal', pl: 2, display: 'flex' }}>
              <KeywordCardList topProducts={topProducts}/>
             </List>
